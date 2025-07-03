92cb0c90f273bf4d5bc116e3749e6dd0dd52126b
devikasaji211 <devikasaji211@gmail.com>
2025-07-03 17:07:11 +0530

    Display

--- ca_articleship_placement/settings.py
+++ ca_articleship_placement/settings.py
@@ -85,9 +85,12 @@ WSGI_APPLICATION = 'ca_articleship_placement.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True
     )
 }
+
 
 
 # Password validation
