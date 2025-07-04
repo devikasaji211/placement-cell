a9098a7e7b71aad7c75e208e1319d3573a1da387
devikasaji211 <devikasaji211@gmail.com>
2025-07-04 15:13:44 +0530

    Supabase setup

--- ca_articleship_placement/settings.py
+++ ca_articleship_placement/settings.py
@@ -86,13 +86,25 @@ WSGI_APPLICATION = 'ca_articleship_placement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True
-    )
-}
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            conn_health_checks=True
+        )
+    }
+else:
+    # Local fallback to SQLite
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 
