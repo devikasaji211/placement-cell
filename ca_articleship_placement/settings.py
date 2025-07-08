b566e30f9991cc926dc4406bc69f3a4fb4d796ed
devikasaji211 <devikasaji211@gmail.com>
2025-07-08 16:35:39 +0530

    Login

--- ca_articleship_placement/settings.py
+++ ca_articleship_placement/settings.py
@@ -161,3 +161,9 @@ MESSAGE_TAGS = {
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect unauthorized users to custom login page
+LOGIN_URL = '/article/article-login/'
+LOGIN_REDIRECT_URL = '/article/'
+LOGOUT_REDIRECT_URL = '/article/article-login/'
+
