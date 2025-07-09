1941192383403a1312cd3b51d25ff4cb302f3615
devikasaji211 <devikasaji211@gmail.com>
2025-07-09 13:30:48 +0530

    Debug

--- ca_articleship_placement/settings.py
+++ ca_articleship_placement/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y43tkad&qnu=qx*x1dk!q#+8--j0=^@5%^!u-dvamx*vvr^t=*'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
